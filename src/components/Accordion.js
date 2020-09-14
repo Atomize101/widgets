9220ed3dff19124395f4cae293d002274241222d
Chris Patrick <atomize101@gmail.com>
2020-09-14 11:06:23 -0400

    onClick when clicking on title

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 const Accordion = ( { items }) => {
-    const renderedItems = items.map(item => {
-        return <div key={item.title}>
-            <div className="title active">
+    const renderedItems = items.map((item, index) => {
+        return (
+        <React.Fragment key={item.title}>
+            <div className="title active"
+            onClick={() => console.log('Title Clicked', index)}>
                 <i className="dropdown icon"></i>
                     {item.title}
             </div>
             <div className="content active">
-                <p>{item.content}</p>
+                    <p>{item.content}</p>
             </div>
-        </div>
+            </React.Fragment>
+        );
     });
 
     return <div className="ui styled accordion">
