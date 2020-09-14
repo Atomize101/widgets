f1bfb2043b9136fad764a51f160a1c8cdf5a29de
Chris Patrick <atomize101@gmail.com>
2020-09-14 11:41:39 -0400

    useState

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ( { items }) => {
+    // Using this method for function components to utilize useState
+    // Create an array with elements 
+    const [activeIndex, setActiveIndex] = useState(null);
+
     const onTitleClick = (index) => {
-        console.log('title clicked', index)
+        setActiveIndex(index);
     };
 
     const renderedItems = items.map((item, index) => {
@@ -22,6 +26,7 @@ const Accordion = ( { items }) => {
 
     return <div className="ui styled accordion">
         {renderedItems}
+        <h1>{activeIndex}</h1>
     </div>
 }
 
