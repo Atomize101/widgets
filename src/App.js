b7fda4d75bf1e88017b10bc4315709681bedaa0a
Chris Patrick <atomize101@gmail.com>
2020-09-14 10:08:27 -0400

    Creating items array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+    {
+        title: 'React',
+        content: 'Front end Javascript'
+    },
+    {
+        title: 'Using React',
+        content: 'It is nice'
+    },
+    {
+        title: 'Lets learn React',
+        content: 'Starting now'
+    }
+];
 
 export default () => {
-    return <h1>Widgets App</h1>
+    return <div>
+        <Accordion items={items} />
+    </div>
 };
 
