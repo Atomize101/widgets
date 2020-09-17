8ba3dfb9c972818fe168aa985e40424c5ffe76ab
Chris Patrick <atomize101@gmail.com>
2020-09-17 11:40:27 -0400

    Setting up useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Search = () => {
     const [term, setTerm] = useState('');
     
+    console.log('testing running');
+    
+    useEffect(() => {
+        console.log('testing');
+    },[term]);
+
     return (
         <div>
            <div className="ui form">
