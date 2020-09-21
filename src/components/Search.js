a540c25a828737bd5e19248fcb65afbf245a7813
Chris Patrick <atomize101@gmail.com>
2020-09-21 18:33:55 -0400

    List to map over snippets

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,41 +1,52 @@
 import Axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('Gaming');
-    const [results, setResults] = useState([]);
-    
-    useEffect(() => {
-        // The initial function cannot be async
-        const search = async () => {
-            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
-                params: {
-                    action: 'query',
-                    list: 'search',
-                    origin: '*',
-                    format: 'json',
-                    srsearch: term
-                },
-            });
-            setResults(data.query.search);
-        }
-            search();
-    }, [term]);
+	const [term, setTerm] = useState('Gaming');
+	const [results, setResults] = useState([]);
 
-    return (
-        <div>
-           <div className="ui form">
-               <div className="field">
-                   <label>Enter Search Term</label>
-                   <input
-                    value={term}
-                    onChange={(e) => setTerm(e.target.value)}
-                    className="input"></input>
-               </div>
-           </div>
-        </div>
-    );
+	useEffect(() => {
+		// The initial function cannot be async
+		const search = async () => {
+			const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+				params: {
+					action: 'query',
+					list: 'search',
+					origin: '*',
+					format: 'json',
+					srsearch: term,
+				},
+			});
+			setResults(data.query.search);
+		};
+		search();
+	}, [term]);
+
+	const renderedResults = results.map((result) => {
+		return (
+			<div className="item">
+				<div className="content">
+					<div className="header">
+                        {result.title}
+                    </div>
+                    {result.snippet}
+				</div>
+			</div>
+		);
+	});
+
+	return (
+		<div>
+			<div className="ui form">
+				<div className="field">
+					<label>Enter Search Term</label>
+					<input value={term} onChange={(e) => setTerm(e.target.value)} className="input"></input>
+				</div>
+			</div>
+    <div className="ui called list">{renderedResults}</div>
+		</div>
+	);
 };
 
 export default Search;
