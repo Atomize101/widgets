e352dbbf57fb739e17f3daa7e0f1552fb8bb38f3
Chris Patrick <atomize101@gmail.com>
2020-09-22 13:53:39 -0400

    Stop searching when search box is empty

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -20,16 +19,21 @@ const Search = () => {
 			});
 			setResults(data.query.search);
 		};
-		search();
+
+		if (term) {
+			search();
+		}
 	}, [term]);
 
 	const renderedResults = results.map((result) => {
 		return (
 			<div key={result.pageid} className="item">
 				<div className="right floated content">
-                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
-                </div>
-                <div className="content">
+					<a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>
+						Go
+					</a>
+				</div>
+				<div className="content">
 					<div className="header">{result.title}</div>
 					<span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
 				</div>
