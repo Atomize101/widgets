d21ea14b1cb7af987ac3c5bb318cc14c4090e76c
Chris Patrick <atomize101@gmail.com>
2020-10-04 12:57:24 -0400

    Starting dropdown component/widget

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
-    {
-        title: 'React',
-        content: 'Front end Javascript'
-    },
-    {
-        title: 'Using React',
-        content: 'It is nice'
-    },
-    {
-        title: 'Lets learn React',
-        content: 'Starting now'
-    }
+	{
+		title: 'React',
+		content: 'Front end Javascript',
+	},
+	{
+		title: 'Using React',
+		content: 'It is nice',
+	},
+	{
+		title: 'Lets learn React',
+		content: 'Starting now',
+	},
 ];
 
 export default () => {
-    return <div>
-        <Search />
-    </div>
+	return (
+		<div>
+			<Dropdown />
+		</div>
+	);
 };
-
