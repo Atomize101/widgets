ceab85cbe8d23f03013f824d31cc3abcd3392612
Chris Patrick <atomize101@gmail.com>
2020-10-12 10:00:56 -0400

    Adding input for language text

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,29 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Dropdown from './Dropdown';
 
 const options = [
-    {
-        label: 'Afrikaans',
-        value: 'af',
-    },
-    {
-        label: 'Arabic',
-        value: 'ar',
-    },
-    {
-        label: 'Hindi',
-        value: 'hi'
-    }
+	{
+		label: 'Afrikaans',
+		value: 'af',
+	},
+	{
+		label: 'Arabic',
+		value: 'ar',
+	},
+	{
+		label: 'Hindi',
+		value: 'hi',
+	},
 ];
 
-const Translate = ()=> {
-    const [language, setLanguage] = useState(options[0]);
-    
-    return (
-      <div>
-          <Dropdown selected={language} onSelectedChange={setLanguage} options={options} />
-      </div>  
-    );
-}
+const Translate = () => {
+	const [language, setLanguage] = useState(options[0]);
+	const [text, setText] = useState('');
+
+	return (
+		<div>
+			<div className="ui form">
+				<div className="field">
+					<label>Enter Text</label>
+					<input value={text} onChange={(e) => setText(e.target.value)}></input>
+				</div>
+			</div>
+			<Dropdown label="Select a language" selected={language} onSelectedChange={setLanguage} options={options} />
+		</div>
+	);
+};
 
 export default Translate;
