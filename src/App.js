bf53ea628715b63e155e26c7a7185ed526aa116e
Chris Patrick <atomize101@gmail.com>
2020-10-15 12:46:29 -0400

    pass dropdown props

--- src/App.js
+++ src/App.js
@@ -35,32 +35,10 @@ const options = [
 	},
 ];
 
-const showAccordion = () => {
-    if(window.location.pathname === '/') {
-        return <Accordion items={items} />
-    }
-}
-
-const showList = () => {
-    if(window.location.pathname === '/list') {
-        return <Search />
-    }
-}
-
-const showDropdown = () => {
-    if(window.location.pathname === '/dropdown') {
-        return <Dropdown />
-    }
-}
-
-const showTranslate = () => {
-    if(window.location.pathname === '/translate') {
-        return <Translate />
-    }
-}
-
 export default () => {
-	return (
+    const [selected, setSelected] = useState(options[0]);
+
+    return (
 		<div>
            <Route path="/">
                <Accordion items={items} />
@@ -69,7 +47,12 @@ export default () => {
                <Search />
            </Route>
            <Route path="/dropdown">
-               <Dropdown />
+               <Dropdown
+                label="Select a color"
+                options={options}
+                selected={selected}
+                onSelectedChange={setSelected}
+               />
            </Route>
            <Route path="/translate">
                <Translate />
